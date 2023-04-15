89ae4715a7a5a840302632b7d94ba416fc8fdd52
Seraphiel97 <seth.graves@outlook.com>
2023-04-15 09:14:50 -0400

    add nav functionality and utilities functionality

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Nav() {
+export default function Nav({user, updateUser}) {
+  
+  function handleLogout() {
+    userService.logOut();
+    updateUser(null);
+  }
+  
   return (
-    <div>Nav</div>
+    <nav>
+      { user ?
+      <>
+        <Link to="/">Home</Link>
+        &nbsp; | &nbsp;
+        <Link to="/services">Services</Link>
+        &nbsp; | &nbsp;
+        <Link to="/appointment">Request Appointment</Link>
+        &nbsp; | &nbsp;
+        <Link to="/appointment/history">Appointment History</Link>
+        &nbsp; | &nbsp;
+        <p>Welcome, {user.name}</p>
+        &nbsp; | &nbsp;
+        <Link to="" onClick={handleLogout}>Logout</Link>
+      </>
+      :
+      <>
+        <Link to="/">Home</Link>
+        &nbsp; | &nbsp;
+        <Link to="/services">Services</Link>
+        &nbsp; | &nbsp;
+        <Link to="/appointment">Request Appointment</Link>
+        &nbsp; | &nbsp;
+        <Link to="/auth">Login</Link>
+      </>
+      }
+    </nav>
   )
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
+import { getUser } from '../../utilities/users-service';
 import Auth from '../Auth/Auth'
 import Nav from '../../components/Nav/Nav'
 import Home from '../Home/Home'
 import Services from '../Services/Services'
 import Appointment from '../Appointment/Appointment'
+import AppointmentHistory from '../AppointmentHistory/AppointmentHistory'
 import './App.css'
 
 function App() {
  
+  const [user, setUser] = useState(getUser())
+
+  function updateUser(userState){
+    setUser(userState)
+  }
 
   return (
     <main className="App">
       <>
-        <Nav />
+        <Nav user={user} updateUser={updateUser}/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/appointment" element={<Appointment />} />
+          <Route path="/appointment/history" element={<AppointmentHistory />} />
         </Routes>
       </>
     </main>
