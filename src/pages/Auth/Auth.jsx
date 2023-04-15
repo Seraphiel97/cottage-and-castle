a0b823236da08cf82c57bcc7451c9a4084933e47
Seraphiel97 <seth.graves@outlook.com>
2023-04-15 10:05:55 -0400

    add UI for auth

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
+import Login from '../../components/Login/Login'
+import RegisterAcct from '../../components/RegisterAcct/RegisterAcct'
 
 export default function Auth() {
+  const [showLogin, setShowLogin] = useState(true)
+  
   return (
-    <div>Auth</div>
+    <main>
+      {showLogin 
+      ?
+      <Login />
+      :
+      <RegisterAcct />
+      }
+      <button onClick={() => setShowLogin(!showLogin)}>
+        {showLogin
+        ?
+        `Don't have an account?`
+        :
+        'Already have an Account?'}
+      </button>
+    </main>
   )
 }
