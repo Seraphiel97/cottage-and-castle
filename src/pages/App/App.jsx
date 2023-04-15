b05cfea603047eb7196300f8909ca81ead80902b
Seraphiel97 <seth.graves@outlook.com>
2023-04-15 08:51:45 -0400

    complete setup of App.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Auth from '../Auth/Auth'
+import Nav from '../../components/Nav/Nav'
+import Home from '../Home/Home'
+import Services from '../Services/Services'
+import Appointment from '../Appointment/Appointment'
 import './App.css'
 
 function App() {
  
 
   return (
-    <div className="App">
-      
-    </div>
+    <main className="App">
+      <>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/appointment" element={<Appointment />} />
+        </Routes>
+      </>
+    </main>
   )
 }
 
