4547c6c4f130ef682521ad1967f96607dfce2ded
Seraphiel97 <seth.graves@outlook.com>
2023-04-20 12:53:16 -0400

    complete home page styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,63 +3,69 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center text-center">
-      <h1>Cottage & Castle (logo)</h1>
       <div>
-        <img src="./src/assets/kitchen.jpg" className="max-w-md md:max-w-2xl"/>
-        <h2>Mission Statement</h2>
+        <img src="./src/assets/businessLogo.png" className="w-full"/>
       </div>
-      <p>
-        At Cottage & Castle, we believe that cleaning is more than just a 
-        chore. It's a magical transformation that turns a messy and chaotic 
-        space into a beautiful and sparkling retreat. We use our cleaning 
-        powers to create a sense of wonder and enchantment in every home we 
-        clean.
-      </p>
-      <p>
-        Our team of skilled cleaners is like a group of fairy godmothers, 
-        bringing a touch of magic to every corner of your home. We wave our 
-        magic wands, and your living room becomes a cozy haven, your kitchen 
-        a place of delicious smells and warmth, and your bedroom a serene 
-        sanctuary.
-      </p>
-      <p>
-        Just like magic, our cleaning services are tailored to meet your 
-        unique needs and preferences. Whether you live in a cozy cottage or 
-        a majestic castle, we have the skills and expertise to transform your 
-        space into a place of beauty and enchantment.
-      </p>
-      <p>
-        At Cottage & Castle, we understand that your home is your kingdom, 
-        and we're here to help you reign over it. Let us sprinkle a little 
-        bit of magic into your life and experience the joy and wonder of a 
-        clean and sparkling home.
-      </p>
-      <h2>Reviews</h2>
-      <div className="md:inline-flex">
-        <div>
-          <img src="./src/assets/apartments.jpg" className="max-w-xs mx-1 rounded-lg"/>
-          <h3>Dennis Byrd</h3>
+      <div className="m-6 p-6 bg-gold bg-opacity-80 rounded-xl text-white">
+        <h2 >Mission Statement</h2>
+        <div className="px-6">
           <p>
+            At Cottage & Castle, we believe that cleaning is more than just a 
+            chore. It's a magical transformation that turns a messy and chaotic 
+            space into a beautiful and sparkling retreat. We use our cleaning 
+            powers to create a sense of wonder and enchantment in every home we 
+            clean.
+          </p>
+          <br/>
+          <p>
+            Our team of skilled cleaners is like a group of fairy godmothers, 
+            bringing a touch of magic to every corner of your home. We wave our 
+            magic wands, and your living room becomes a cozy haven, your kitchen 
+            a place of delicious smells and warmth, and your bedroom a serene 
+            sanctuary.
+          </p>
+          <br/>
+          <p>
+            Just like magic, our cleaning services are tailored to meet your 
+            unique needs and preferences. Whether you live in a cozy cottage or 
+            a majestic castle, we have the skills and expertise to transform your 
+            space into a place of beauty and enchantment.
+          </p>
+          <br/>
+          <p>
+            At Cottage & Castle, we understand that your home is your kingdom, 
+            and we're here to help you reign over it. Let us sprinkle a little 
+            bit of magic into your life and experience the joy and wonder of a 
+            clean and sparkling home.
+          </p>
+        </div>
+      </div>
+      <h2>Reviews</h2>
+      <div className="justify-center md:inline-flex">
+        <div className="mb-6 m-1 p-3 bg-indigo text-white rounded-xl">
+          <img src="./src/assets/apartments.jpg" className="max-w-md ml-4 px-16 pb-3 md:max-w-xs mx-1 rounded-lg"/>
+          <h3>Dennis Byrd</h3>
+          <p className="px-16">
             "I was extremely satisfied with the service provided by Cottage & Castle. They exceeded my expectations, and I would 
             highly recommend them to anyone looking for a reliable and 
             thorough cleaning service. I will definitely be using them again 
             in the future."
           </p>
         </div>
-        <div>
-          <img src="./src/assets/farmhouse.jpg" className="max-w-xs mx-1 rounded-lg"/>
+        <div className="mb-6 m-1 p-3 bg-indigo text-white rounded-xl">
+          <img src="./src/assets/farmhouse.jpg" className="max-w-md ml-4 px-16 pb-3 md:max-w-xs mx-1 rounded-lg"/>
           <h3>Michael Baker</h3>
-          <p>
+          <p className="px-16">
             "What impressed me the most was how they took care of the 
             hard-to-reach areas, such as the ceiling fans, window sills, 
             and baseboards. They even cleaned the blinds and the inside 
             of the microwave, which I had never thought of cleaning before."
           </p>
         </div>
-        <div>
-          <img src="./src/assets/largehome.jpg" className="max-w-xs mx-1 rounded-lg"/>
+        <div className="mb-6 m-1 p-3 bg-indigo text-white rounded-xl">
+          <img src="./src/assets/largehome.jpg" className="max-w-md ml-4 px-16 pb-3 md:max-w-xs mx-1 rounded-lg"/>
           <h3>Lee Brown</h3>
-          <p>
+          <p className="px-16">
             "I was really impressed with their work. They arrived on time and 
             were very friendly and professional throughout the entire process.
             They were thorough in their cleaning, and I could tell that 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+      colors: {
+        'gold': '#B3936F',
+        'teal': '#6fb1b3',
+        'indigo': '#716fb3',
+        'white': '#ffffff',
+        'black': '#000000',
+      },
       extend: {},
     },
   plugins: [
