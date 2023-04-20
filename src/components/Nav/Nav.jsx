34c75bd206773f5d8ac6dee07c0bc33ae380e957
Seraphiel97 <seth.graves@outlook.com>
2023-04-20 17:46:17 -0400

    run build for push

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,6 @@
 import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import * as usersService from '../../utilities/users-service';
-import {HiMenu} from "react-icons/hi"
 
 export default function Nav({user, updateUser}) {
   
