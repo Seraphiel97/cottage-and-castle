9cecef177c2f56b0a46081cd7827551436d0fc66
Seraphiel97 <seth.graves@outlook.com>
2023-04-21 10:10:47 -0400

    fix rendering bug

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import {useState} from 'react'
 import Login from '../../components/Login/Login'
 import RegisterAcct from '../../components/RegisterAcct/RegisterAcct'
 
 export default function Auth({setUser, user}) {
+  const [showLogin, setShowLogin] = useState(true)
   
   return (
     <main className="flex flex-col justify-center items-center">
